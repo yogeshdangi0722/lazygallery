7fa6f6f6966699b71a42d2478a5a22a61828c532
Yogesh Kumar Dangi <yogeshdangi0722@gmail.com>
2023-02-10 14:06:56 +0530

    title added

--- src/component/Searchbar.js
+++ src/component/Searchbar.js
@@ -6,7 +6,7 @@ const Searchbar = () => {
     const backProp= {
         backgroundImage: `url('https://mdbootstrap.com/img/new/standard/city/041.jpg')`,
         backgroundSize: "cover",
-        height: '80vh',
+        height: '50vh',
         backgroundRepeat:'no-repeat',
         marginBottom:'15px',
       }
@@ -16,7 +16,7 @@ const {setSearchTerm} = useGlobalContext();
       
 
   return (
-    <div class="bg-image mw-100 d-flex justify-content-center align-items-center" 
+    <div class="bg-image mw-100 d-flex justify-content-center align-items-center " 
             style={backProp}>
            <input class="form-control w-75" type="text" placeholder="Search Here" onChange={(e)=>{setSearchTerm(e.target.value)}} />
     </div>
